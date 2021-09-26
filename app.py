fbece3065337fe4b768791144b490aa2bb1128b5
Dodoko <47654458+harizaldycahya@users.noreply.github.com>
2021-09-27 01:12:34 +0700

    Remove footer

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 st.title('hello streamlit')
 st.subheader('welcome to my first streamlit app')
+
+# remove footer
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
